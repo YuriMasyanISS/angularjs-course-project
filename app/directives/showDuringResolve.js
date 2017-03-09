33e0e620a5542a0733aa6d415a2ea0a31adb792c
Stepan Suvorov <stevermeister@gmail.com>
2017-03-09 20:06:27 +0100

    Update showDuringResolve.js

--- app/directives/showDuringResolve.js
+++ app/directives/showDuringResolve.js
@@ -8,16 +8,8 @@ angular.module('app').directive('showDuringResolve', function($rootScope) {
     link: function(scope, element) {
 
       element.addClass('ng-hide');
-      $rootScope.$on('$stateChangeStart',
-        function(event, toState, toParams, fromState, fromParams, options){ element.removeClass('ng-hide') });
-      $rootScope.$on('$stateChangeSuccess',
-        function(event, toState, toParams, fromState, fromParams, options){ element.addClass('ng-hide') });
-
-      // let unregister = $rootScope.$on('$routeChangeStart', function() {
-      //   element.removeClass('ng-hide');
-      // });
-      //
-      // scope.$on('$destroy', unregister);
+      $rootScope.$on('$stateChangeStart', () =>  element.removeClass('ng-hide'));
+      $rootScope.$on('$stateChangeSuccess',() => element.addClass('ng-hide'));
     }
   };
 });
